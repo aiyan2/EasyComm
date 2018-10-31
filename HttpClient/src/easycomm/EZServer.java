7a0213dd8cb24a100f1d090511678b2e1bd79aba
john <john@win-99.devqa2.net>
2018-10-31 15:04:12 -0700

    submitted after refined the workflow  thanks to YingLi, also the super client updated accordingly.

--- HttpClient/src/easycomm/EZServer.java
+++ HttpClient/src/easycomm/EZServer.java
@@ -3,18 +3,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.util.Date;
-
-import com.fortinet.devqa.Util;
 
 /**
- * Java program to create a simple HTTP Server to demonstrate how to use
- * ServerSocket and Socket class.
- *  
+ *2018-10-31: reproduced the issues of 2nd post blocked by FGT if av enabled
+ *
+ * Client: SuperClient + EditfromWireShark.java ( for mesgs)
+ * 
+ * src-location: https://github.com/aiyan2/EasyComm/tree/master/HttpClient/src/easycomm 
+ * 
  */
 public class EZServer {
 	
@@ -65,44 +63,7 @@ public class EZServer {
 	        
             +"\r\n" 
      		;		
-     static String auth_401_from_customer_internaL = "HTTP/1.1 401 Unauthorized\r\n" + 
-     		"Content-Type: text/html\r\n" + 
-     		"Server: Microsoft-IIS/7.5\r\n" + 
-     		"WWW-Authenticate: Negotiate\r\n" + 
-     		"WWW-Authenticate: NTLM\r\n" + 
-     		"X-Powered-By: ASP.NET\r\n" + 
-     		"Date: Mon, 15 Oct 2018 14:42:38 GMT\r\n" + 
-     		"Content-Length: 1293\r\n" + 
-     		"\r\n" + 
-     		"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\r\n" + 
-     		"<html xmlns=\"http://www.w3.org/1999/xhtml\">\r\n" + 
-     		"<head>\r\n" + 
-     		"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\"/>\r\n" + 
-     		"<title>401 - Unauthorized: Access is denied due to invalid credentials.</title>\r\n" + 
-     		"<style type=\"text/css\">\r\n" + 
-     		"<!--\r\n" + 
-     		"body{margin:0;font-size:.7em;font-family:Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;}\r\n" + 
-     		"fieldset{padding:0 15px 10px 15px;} \r\n" + 
-     		"h1{font-size:2.4em;margin:0;color:#FFF;}\r\n" + 
-     		"h2{font-size:1.7em;margin:0;color:#CC0000;} \r\n" + 
-     		"h3{font-size:1.2em;margin:10px 0 0 0;color:#000000;} \r\n" + 
-     		"#header{width:96%;margin:0 0 0 0;padding:6px 2% 6px 2%;font-family:\"trebuchet MS\", Verdana, sans-serif;color:#FFF;\r\n" + 
-     		"background-color:#555555;}\r\n" + 
-     		"#content{margin:0 0 0 2%;position:relative;}\r\n" + 
-     		".content-container{background:#FFF;width:96%;margin-top:8px;padding:10px;position:relative;}\r\n" + 
-     		"-->\r\n" + 
-     		"</style>\r\n" + 
-     		"</head>\r\n" + 
-     		"<body>\r\n" + 
-     		"<div id=\"header\"><h1>Server Error</h1></div>\r\n" + 
-     		"<div id=\"content\">\r\n" + 
-     		" <div class=\"content-container\"><fieldset>\r\n" + 
-     		"  <h2>401 - Unauthorized: Access is denied due to invalid credentials.</h2>\r\n" + 
-     		"  <h3>You do not have permission to view this directory or page using the credentials that you supplied.</h3>\r\n" + 
-     		" </fieldset></div>\r\n" + 
-     		"</div>\r\n" + 
-     		"</body>\r\n" + 
-     		"</html>";
+     
 
 	public static void main(String args[]) throws IOException {
 
@@ -117,23 +78,22 @@ public class EZServer {
 				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				OutputStream os = socket.getOutputStream();
 
-				boolean step1 = false; // flag for 2 steps to handle auth-request ( pattern in the middle, not in end).
+//				boolean step1 = false; // flag for 2 steps to handle auth-request ( pattern in the middle, not in end).
 				try {
 					while ((request = br.readLine()) != null) {
-//                if ((request = br.readLine()) != null) {
+
 						System.out.println("Rvd:" + request);
 
-						if (request.matches("Expect: 100-continue"))
-							sendHttpResponse(os, countinue_100, "100-continue");
-
-						else if (request.matches("------WebKitFormBoundarydRcL8NeqnnjYDT3i--")) // ending of payload
+						if (request.matches("Expect: 100-continue")) // get the first POST
 							sendHttpResponse(os, auth_401, "401-auth");
 
-						else if (request.matches("Authorization: Basic bmdpbng6MTIzNA=="))
-							step1 = true;
+						if (request.startsWith("Authorization: "))
+//							step1 = true;
+//						if (request.startsWith("Accept-Language:") // end of auth-mesg
+//								&& step1)
+							sendHttpResponse(os, countinue_100, "100-continue");
 
-						else if (request.startsWith("Accept-Language:") // end of auth-mesg
-								&& step1)
+						if (request.matches("------WebKitFormBoundarydRcL8NeqnnjYDT3i--")) // ending of payload
 							sendHttpResponse(os, ok_200, "200-OK");
 
 //                	 else 
